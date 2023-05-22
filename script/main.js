a6aab0f2db4ca09ca7b97a28747f4d990486fa7e
no3utufff89 <14198908@mail.rugit config --global core.autocrlf truegit config --global core.safecrlf warngit config --global core.quotepath offgit add .git statusgit config --global user.email 14198908@mail.ru>
2023-05-23 00:12:51 +0300

    swiper

--- script/main.js
+++ script/main.js
@@ -1,6 +1,7 @@
 import controls from "./controls.js";
 import {acc} from "./acc.js";
 import {animate, styleBuilder} from "./animateMenu.js";
+
 controls();
 acc();
 
@@ -14,6 +15,14 @@ window.addEventListener('resize', () => {
 //     styleBuilder(pageWidth);
 //
 // })
+new Swiper('.swiper', {
+    slidesPerView: 1,
+    loop: true,
+    navigation: {
+        nextEl: '.slider-button-next',
+        prevEl: '.slider-button-prev',
+    },
+});
 
 
 
